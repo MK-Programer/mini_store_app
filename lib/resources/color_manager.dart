c1267c3901b4b0cd01d4f616c1eb52fac40c4b94
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-09-03 01:17:40 +0200

    Setting up the application theme and structure

---
+++ lib/resources/color_manager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color lightScaffoldColor = const Color(0xFFF8F8F8);
+  static Color lightCardColor = const Color(0xFFFfFFFf);
+  static Color lightBackgroundColor = const Color(0xFFD0E8F2);
+  static Color lightIconsColor = const Color.fromARGB(255, 175, 0, 120);
+  static Color lightTextColor = const Color(0xff324558);
+  static Color blackColor = Colors.black;
+  static Color blueColor = Colors.blue;
+}
---
+++ lib/resources/theme_manager.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:mini_store_app/resources/color_manager.dart';
+import 'package:mini_store_app/resources/font_manager.dart';
+import 'package:mini_store_app/resources/values_manager.dart';
+
+class ThemeManager {
+  static ThemeData themeData() {
+    return ThemeData(
+      // textTheme: TextTheme(),
+      scaffoldBackgroundColor: ColorManager.lightScaffoldColor,
+      primaryColor: ColorManager.lightCardColor,
+      backgroundColor: ColorManager.lightBackgroundColor,
+      appBarTheme: AppBarTheme(
+        iconTheme: IconThemeData(
+          color: ColorManager.lightIconsColor,
+        ),
+        backgroundColor: ColorManager.lightScaffoldColor,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          color: ColorManager.lightTextColor,
+          fontSize: FontSize.s22,
+          fontWeight: FontWeightManager.bold,
+        ),
+        elevation: AppSize.s0,
+      ),
+      iconTheme: IconThemeData(
+        color: ColorManager.lightIconsColor,
+      ),
+      textSelectionTheme: TextSelectionThemeData(
+        cursorColor: ColorManager.blackColor,
+        selectionColor: ColorManager.blueColor,
+        // selectionHandleColor: Colors.blue,
+      ),
+      // textTheme: TextTheme()
+      // textTheme: Theme.of(context).textTheme.apply(
+      //       bodyColor: Colors.black,
+      //       displayColor: Colors.black,
+      //     ),
+      cardColor: ColorManager.lightCardColor,
+      // status bar
+      brightness: Brightness.light,
+      colorScheme: ThemeData().colorScheme.copyWith(
+            secondary: ColorManager.lightIconsColor,
+            brightness: Brightness.light,
+          ),
+    );
+  }
+}
