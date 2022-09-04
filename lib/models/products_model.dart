40bb12f1b4622b5750c7c596f4122eb8d42bf1c3
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-09-04 08:01:47 +0200

    Convert the json to a dart class model

---
+++ lib/models/products_model.dart
@@ -0,0 +1,42 @@
+import 'categories_model.dart';
+
+class ProductsModel {
+  int? id;
+  String? title;
+  int? price;
+  String? description;
+  CategoriesModel? category;
+  List<String>? images;
+
+  ProductsModel(
+      {this.id,
+      this.title,
+      this.price,
+      this.description,
+      this.category,
+      this.images});
+
+  ProductsModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    price = json['price'];
+    description = json['description'];
+    category = json['category'] != null
+        ? CategoriesModel.fromJson(json['category'])
+        : null;
+    images = json['images'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['id'] = id;
+    data['title'] = title;
+    data['price'] = price;
+    data['description'] = description;
+    if (category != null) {
+      data['category'] = category!.toJson();
+    }
+    data['images'] = images;
+    return data;
+  }
+}
